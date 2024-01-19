380475ff8288a44943fcd61243770d5fc431a52d
Shahir <shahirp70@gmail.com>
2024-01-19 19:29:35 +0530

    none

---
+++ lib/auth/Signin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 100,
+        width: 100,
+        color: Colors.greenAccent,
+      ),
+    );
+  }
+}
